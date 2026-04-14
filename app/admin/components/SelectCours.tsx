64b4ed1cd17089f023c85deab76bd118f280b106
Kim Lavoie <kim.lavoie.14@gmail.com>
2026-04-14 15:59:44 -0400

    Ajouté un composant SelectCours

---
+++ app/admin/components/SelectCours.tsx
@@ -0,0 +1,22 @@
+import { db } from "@/app/db/db"
+import { useLiveQuery } from "dexie-react-hooks"
+import { useEffect, useState } from "react"
+
+export default function({value, onChange, saison}: any){
+    const [coursListe, setCoursListe] = useState([])
+
+    useEffect(() => {
+        db.cours.where('saison').equals(saison).toArray().then((cours:any) => {
+            setCoursListe(cours)
+        })
+    }, [saison])
+    
+    return <>
+        <label>Cours: <select name="cours" value={value} onChange={(ev) => onChange(Number(ev.target.value))}>
+            <option value="0" hidden disabled>Choisissez un cours</option>
+            {coursListe?.map((cour: any) => (
+                <option key={cour.id} value={cour.id}>{cour.sigle} - {cour.nom}</option>
+            ))}
+        </select></label>
+    </>
+}
--- app/admin/priorites/ajout/page.tsx
+++ app/admin/priorites/ajout/page.tsx
@@ -5,35 +5,19 @@ import { useLiveQuery } from "dexie-react-hooks"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 import SelectEnseignant from "../../components/SelectEnseignant"
-
-interface Priorite{
-    id: number
-    enseignant: number
-    cours: number
-    sessionDebut: number
-}
+import SelectCours from "../../components/SelectCours"
 
 export default function(){
     const [enseignant, setEnseignant] = useState(0)
     const [cours, setCours] = useState(0)
     const [sessionDebut, setSessionDebut] = useState(0)
     const [saison, setSaison] = useState("automne")
-    const [coursListe, setCoursListe] = useState([])
 
     const sessions = useLiveQuery(() => db.sessions.toArray())
 
     useEffect(() => {
         setSessionDebut(sessions?.[0]?.id ?? 0)
     }, [sessions])
-
-
-
-    useEffect(() => {
-        db.cours.where('saison').equals(saison).toArray().then((cours:any) => {
-            setCoursListe(cours)
-            setCours(cours[0]?.id ?? 0)
-        })
-    }, [saison])
 
     const router = useRouter()
 
@@ -44,9 +28,8 @@ export default function(){
         })
         setEnseignant(0)
         //@ts-ignore
-        setCours(coursListe?.[0]?.id ?? 0)
+        setCours(0)
         setSessionDebut(sessions?.[0]?.id ?? 0)
-
     }
 
     async function sessionChanged(ev: any){
@@ -62,11 +45,7 @@ export default function(){
                     <option key={session.id} value={session.id} data-saison={session.saison}>{session.saison} {session.annee}</option>
                 ))}
             </select></label></p>
-            <p><label>Cours: <select name="cours" value={cours} onChange={(ev) => setCours(Number(ev.target.value))}>
-                {coursListe?.map((cour: any) => (
-                    <option key={cour.id} value={cour.id}>{cour.sigle} - {cour.nom}</option>
-                ))}
-            </select></label></p>
+            <p><SelectCours value={cours} onChange={(id: any) => setCours(id)} saison={saison} /></p>
             
             
             <input type="submit" value="Ajouter" />
