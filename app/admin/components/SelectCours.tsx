facedd2d473aefeef43e1e130f31c115ca47da73
Kim Lavoie <kim.lavoie.14@gmail.com>
2026-04-14 16:58:35 -0400

    SelectCours est plus robuste et fonctionne avec les valeurs par défaut

--- app/admin/components/SelectCours.tsx
+++ app/admin/components/SelectCours.tsx
@@ -1,5 +1,4 @@
 import { db } from "@/app/db/db"
-import { useLiveQuery } from "dexie-react-hooks"
 import { useEffect, useState } from "react"
 
 export default function({value, onChange, saison}: any){
@@ -8,9 +7,10 @@ export default function({value, onChange, saison}: any){
     useEffect(() => {
         db.cours.where('saison').equals(saison).toArray().then((cours:any) => {
             setCoursListe(cours)
+            if(value != 0 && cours.find((cour: any) => cour.id == value) == undefined) onChange(0)
         })
     }, [saison])
-    
+
     return <>
         <label>Cours: <select name="cours" value={value} onChange={(ev) => onChange(Number(ev.target.value))}>
             <option value="0" hidden disabled>Choisissez un cours</option>
--- app/admin/priorites/[id]/page.tsx
+++ app/admin/priorites/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useLiveQuery } from "dexie-react-hooks";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import SelectEnseignant from "../../components/SelectEnseignant";
+import SelectCours from "../../components/SelectCours";
 
 export default function(){
     const [id, setId] = useState(-1)
@@ -12,16 +13,8 @@ export default function(){
     const [saison, setSaison] = useState("automne")
     const [cours, setCours] = useState(0)
     const [enseignant, setEnseignant] = useState(0)
-    const [coursListe, setCoursListe] = useState([])
 
     const sessions = useLiveQuery(() => db.sessions.toArray())    
-
-    useEffect(() => {
-        db.cours.where('saison').equals(saison).toArray().then((cours:any) => {
-            setCoursListe(cours)
-            setCours(cours[0]?.id ?? 0)
-        })
-    }, [saison])
 
     useEffect(() => {
         setSaison(sessions?.find((el) => el.id == sessionDebut)?.saison ?? "automne")
@@ -33,6 +26,7 @@ export default function(){
     useEffect(() => {
         db.priorites.get(Number(params.id))
         .then((priorite) => {
+            console.log(priorite)
             setId(priorite?.id ?? -1)
             setEnseignant(priorite?.enseignant ?? 0)
             setCours(priorite?.cours ?? 0)
@@ -61,11 +55,7 @@ export default function(){
                 ))}
             </select></label></p>
 
-            <p><label>Cours: <select name="cours" value={cours} onChange={(ev) => setCours(Number(ev.target.value))}>
-                {coursListe?.map((cour: any) => (
-                    <option key={cour.id} value={cour.id}>{cour.sigle} - {cour.nom}</option>
-                ))}
-            </select></label></p>
+            <p><SelectCours value={cours} onChange={(id: any) => setCours(id)} saison={saison} /></p>
             
             <input type="submit" value="Modifier" />
         </form>
