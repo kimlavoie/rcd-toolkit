294b79460b4cd9c8b80e6e2549ece250a9a826e6
Kim Lavoie <kim.lavoie.14@gmail.com>
2026-04-16 11:38:39 -0400

    Ajouté le composant de formulaire pour les supervisions

--- app/admin/supervisions/[id]/page.tsx
+++ app/admin/supervisions/[id]/page.tsx
@@ -2,45 +2,20 @@
 
 import { db } from "@/app/db/db";
 import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import SelectEnseignant from "../../components/inputs/SelectEnseignant";
-import SelectStage from "../../components/inputs/SelectStage";
+import SupervisionForm from "../../components/forms/SupervisionForm";
 
 export default function(){
-    const [id, setId] = useState(-1)
-    const [enseignant, setEnseignant] = useState(0)
-    const [stage, setStage] = useState(0)
-    const [nbStagiaires, setNbStagiaires] = useState(0)
-
-
     const params = useParams()
     const router = useRouter()
 
-    useEffect(() => {
-        db.supervisions.get(Number(params.id))
-        .then((supervision) => {
-            setId(supervision?.id ?? -1)
-            setEnseignant(supervision?.enseignant ?? 0)
-            setStage(supervision?.stage ?? 0)
-            setNbStagiaires(supervision?.nbStagiaires ?? 0)
-        })  
-    }, [])
-
-    function submit(event: React.SubmitEvent){
-        event.preventDefault()
-        db.supervisions.update(id, {enseignant, stage, nbStagiaires})
+    function submit(supervision: any){
+        const {id, ...rest} = supervision
+        db.supervisions.update(Number(id), rest)
         router.push("../supervisions")
     }
 
     return <>
-        <form onSubmit={submit}>
-            <p><SelectStage value={stage} onChange={(id: any) => setStage(id)} /></p>
-            <p><SelectEnseignant value={enseignant} onChange={(id: any) => setEnseignant(id)} /></p>
-            <p><label>Nombre de stagiaires: <input type="number" name="nbStagiaires" value={nbStagiaires} onChange={(ev) => setNbStagiaires(Number(ev.target.value))} /></label></p>
-
-            
-            <input type="submit" value="Modifier" />
-        </form>
+        <SupervisionForm id={params.id} onSubmit={submit} />
         <button onClick={() => router.push("../supervisions")}>Retour</button>
     </>
     
--- app/admin/supervisions/ajout/page.tsx
+++ app/admin/supervisions/ajout/page.tsx
@@ -2,37 +2,19 @@
 
 import { db } from "@/app/db/db"
 import { useRouter } from "next/navigation"
-import { useState } from "react"
-import SelectEnseignant from "../../components/inputs/SelectEnseignant"
-import SelectStage from "../../components/inputs/SelectStage"
-
-
+import SupervisionForm from "../../components/forms/SupervisionForm"
 
 export default function(){
-    const [enseignant, setEnseignant] = useState(0)
-    const [stage, setStage] = useState(0)
-    const [nbStagiaires, setNbStagiaires] = useState(0)
 
     const router = useRouter()
 
-    function submit(event: React.SubmitEvent){
-        event.preventDefault()
-        db.supervisions.add({
-            enseignant, stage, nbStagiaires
-        })
-        setEnseignant(0)
-        setStage(0)
-        setNbStagiaires(0)
+    function submit(supervision: any, resetForm: any){
+        db.supervisions.add(supervision)
+        resetForm()
     }
 
     return <>
-        <form onSubmit={submit}>
-            <p><SelectStage value={stage} onChange={(id: any) => setStage(id)} /></p>
-            <p><SelectEnseignant value={enseignant} onChange={(id: any) => setEnseignant(id)} /></p>
-            <p><label>Nombre de stagiaires: <input type="number" name="nbStagiaires" value={nbStagiaires} onChange={(ev) => setNbStagiaires(Number(ev.target.value))} /></label></p>
-            
-            <input type="submit" value="Ajouter" />
-        </form>
+        <SupervisionForm onSubmit={submit} />
         <button onClick={() => router.push("../supervisions")}>Retour</button>
     </>
     
