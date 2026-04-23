b84258ffec7ea6e810efb6db2d0a695740872128
Kim Lavoie <KimLavoie@cegepjonquiere.ca>
2026-04-23 09:30:39 -0400

    Ajouté les fonctions d'import et d'export

---
+++ app/db/export/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Link from "next/link"
+import { db } from "../db"
+
+async function pumpDB(){
+    const allData: any = {};
+  
+  // Iterate through each table defined in the Dexie instance
+    await Promise.all(db.tables.map(async table => {
+        allData[table.name] = await table.toArray();
+    }));
+
+  return allData;
+}
+
+async function download(){
+    const data = await pumpDB()
+    const json = JSON.stringify(data)
+    const blob = new Blob([json], {type: 'application/json'})
+    const url = URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.href = url
+    link.download = "data.json"
+    link.click()
+
+    URL.revokeObjectURL(url)
+}
+
+export default function(){    
+    return <>
+        <button onClick={download}>Télécharger les données</button>
+        <Link href="/">Retour à l'accueil</Link>
+    </>
+}
---
+++ app/db/import/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Link from "next/link";
+import { useRef } from "react";
+import { db } from "../db";
+
+export default function(){
+    const ref = useRef<HTMLInputElement>(null)
+
+    async function upload(){
+        const fileTextContent = await ref.current?.files?.[0].text()
+        const fileContent = JSON.parse(fileTextContent!)
+
+        Object.entries(fileContent).forEach(async (table: [string, any]) => {
+            const [name, content] = table
+            //@ts-ignore
+            await db[name].clear()
+            content.forEach(async (entry: any) => {
+                //@ts-ignore
+                await db[name].add(entry)
+            })
+            
+        })
+        alert("Données chargées avec succès")
+    }
+
+    return <>
+        <input type="file" id="fileInput" ref={ref}/>
+        <button onClick={upload}>Charger les données</button>
+        <Link href="/">Retour à l'accueil</Link>
+    </>
+}
