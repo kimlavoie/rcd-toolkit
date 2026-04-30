5be7899103729c50d66d0c43e68223e25cdb367d
Kim Lavoie <KimLavoie@cegepjonquiere.ca>
2026-04-30 11:48:31 -0400

    Facilité l'archivage en mettant la date et l'heure actuelle comme nom par défaut pour l'enregistrement des données

--- app/db/export/page.tsx
+++ app/db/export/page.tsx
@@ -15,13 +15,15 @@ async function pumpDB(){
 }
 
 async function download(){
+    const datetime = new Date().toISOString().split('.')[0]
+
     const data = await pumpDB()
     const json = JSON.stringify(data)
     const blob = new Blob([json], {type: 'application/json'})
     const url = URL.createObjectURL(blob)
     const link = document.createElement('a')
     link.href = url
-    link.download = "data.json"
+    link.download = datetime + ".json"
     link.click()
 
     URL.revokeObjectURL(url)
